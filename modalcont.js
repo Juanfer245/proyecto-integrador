f1869be41d04c11ec4962f6fbe6f6f07c15ba09d
[Michelle Aguirre]git configgit helpgit config --global user.name [Michelle <[mkp.aguirre@yavirac.edu.ec]>
2022-07-27 01:22:36 -0500

    carpetas

--- modalcont.js
+++
@@ -1,9 +0,0 @@
-Swal.fire({
-    title: 'Custom animation with Animate.css',
-    showClass: {
-      popup: 'animate__animated animate__fadeInDown'
-    },
-    hideClass: {
-      popup: 'animate__animated animate__fadeOutUp'
-    }
-  })
